ea83f0008e8d0725afb36864b133d083a9b83b33
Christoph Stahl <christoph.stahl@tu-dortmund.de>
2026-02-18 14:28:21 +0100

    docs: Use Google style docs for player_libmpv.py

--- syng/player_libmpv.py
+++ syng/player_libmpv.py
@@ -1,9 +1,10 @@
+"""Capsules the mpv based player."""
+
 import asyncio
 import locale
 import os
 import sys
 from collections.abc import Callable, Iterable
-from enum import Enum
 from typing import cast
 
 import mpv
@@ -15,6 +16,18 @@ from syng.runningstates import Lifecycle, RunningState
 
 
 class Player:
+    """MPV based player to play the media.
+
+    This communicates with mpv through libmpv. The player itself runs in a seperate process.
+    It also generates and shows a qr code in a corner.
+
+    The player window should be open for as long, as a connection to the server exists. If no song
+    is currently playing, it will show a static image. Before every song, a "Next up" screen is
+    shown for some seconds. When the window is closed, the client also disconnects.
+
+    At the end of every song, the next song is previewed through a pop up.
+    """
+
     def __init__(
         self,
         config: ClientConfig,
@@ -22,6 +35,18 @@ class Player:
         connection_state: RunningState,
         queue: list[Entry] | None = None,
     ) -> None:
+        """Initialize the class and set internal attributes.
+
+        This sets the livecycle of MPV to STARTING.
+
+        Args:
+            config: The configuration of the client. This is mainly used to render the qr code
+            quit_callback: Function, that is called, when the player window is closed.
+            connection_state: Reference to the state of the components of syng. Will be
+                manipulated by this class.
+            queue: Reference to the queue of the client for the "next up" pop-up
+
+        """
         locale.setlocale(locale.LC_ALL, "C")
         qr_string = f"{config.general.server}/{config.general.room}"
         self.connection_state = connection_state
@@ -47,6 +72,11 @@ class Player:
         self.callback_audio_load: str | None = None
 
     def start(self) -> None:
+        """Start the mpv process.
+
+        This also sets and registers some internal callbacks. At the end of the method the
+        lifecycle of MPV ist STARTED.
+        """
         self.mpv = mpv.MPV(
             ytdl=True,
             input_default_bindings=True,
@@ -74,6 +104,16 @@ class Player:
         self.connection_state.set_mpv_state_no_lock(Lifecycle.STARTED)
 
     def playtime_remaining_handler(self, attribute: str, value: float) -> None:
+        """Update the "next up" pop-up, if at the end of a song.
+
+        This handles animation by setting the y position according to the playtime remaining.
+        If this is the last song in the queue, this does nothing.
+
+        Args:
+            attribute: Unused
+            value: `playtime-remaining` as a float in seconds.
+
+        """
         if self.mpv is None:
             print("MPV is not initialized", file=sys.stderr)
             return
@@ -100,6 +140,17 @@ class Player:
         )
 
     def event_callback(self, event: mpv.MpvEvent) -> None:
+        """Handle events of MPV.
+
+        This listens on the ``shutdown`` and ``file-loaded`` event.
+        For ``shutdown``, it calls the quit_callback function, for ``file-loaded``, it ensures, that
+        accompaning audio tracks are also loaded, when an entry has a seperate audio and
+        video file.
+
+        Args:
+            event: MPV event.
+
+        """
         e = event.as_dict()
         if e["event"] == b"shutdown":
             self.quit_callback()
@@ -112,12 +163,27 @@ class Player:
             self.callback_audio_load = None
 
     def update_qr(self, qr_string: str) -> None:
+        """Update the QR code.
+
+        Args:
+            qr_string: String, that will be shown as the qr code.
+
+        """
         qr = QRCode(box_size=self.qr_box_size, border=1)
         qr.add_data(qr_string)
         qr.make()
         self.qr = qr.make_image().convert("RGBA")
 
     def osd_size_handler(self, attribute: str, value: int) -> None:
+        """Handle resize events of the player.
+
+        Ensures, that the position of the qr code is always in a corner.
+
+        Args:
+            attribute: Unused
+            value: Unused
+
+        """
         if self.mpv is None:
             print("MPV is not initialized", file=sys.stderr)
             return
@@ -144,6 +210,17 @@ class Player:
         self.qr_overlay = self.mpv.create_image_overlay(self.qr, pos=(x_pos, y_pos))
 
     async def queue_next(self, entry: Entry) -> None:
+        """Generate and show the "next up" screen.
+
+        This is done by showing a static image and printing subtitles with the appropriate
+        information in the middle of the screen, for a (configurable) number of seconds.
+
+        This function blocks, until the screen is done showing.
+
+        Args:
+            entry: The entry, that is next.
+
+        """
         if self.mpv is None:
             print("MPV is not initialized", file=sys.stderr)
             return
@@ -173,6 +250,14 @@ class Player:
             self.quit_callback()
 
     def play_image(self, image: str, duration: int, sub_file: str | None = None) -> None:
+        """Play a static image.
+
+        Args:
+            image: Path to the image
+            duration: Duration to show the image
+            sub_file: Path to a subtitle file, or ``None``
+
+        """
         if self.mpv is None:
             print("MPV is not initialized", file=sys.stderr)
             return
@@ -193,6 +278,17 @@ class Player:
         audio: str | None = None,
         override_options: dict[str, str] | None = None,
     ) -> None:
+        """Play a video file.
+
+        If given, use ``audio`` as the audio track. Otherwise use the audio from the video.
+
+        Args:
+            video: Path to the video file
+            audio: Path to the audio file or ``None``
+            override_options: Dictionary with mpv configurations, that will be applied for this
+                playback, or ``None``
+
+        """
         if self.mpv is None:
             print("MPV is not initialized", file=sys.stderr)
             return
@@ -221,6 +317,7 @@ class Player:
             self.quit_callback()
 
     def skip_current(self) -> None:
+        """Skip the currently playing entry."""
         if self.mpv is None:
             print("MPV is not initialized", file=sys.stderr)
             return
