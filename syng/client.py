f4d479f58d41e3df10aff232a6a39c8542e36d59
Christoph Stahl <christoph.stahl@tu-dortmund.de>
2026-05-28 21:05:20 +0200

    fix(client): Call the correct function for resume (resume() and not play())

--- syng/client.py
+++ syng/client.py
@@ -255,7 +255,7 @@ class Client:
 
         match data.get("command"):
             case "resume":
-                self.player.play()
+                self.player.resume()
             case "pause":
                 self.player.pause()
 
