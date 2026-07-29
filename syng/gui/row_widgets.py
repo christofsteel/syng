5b6b4f123e5b2046a7b4cdc707f36c63f52962ca
nachtpfoetchen <nachtpfoetchen@posteo.de>
2026-07-29 19:55:12 +0200

    feat: added hover help texts on labels

--- syng/gui/row_widgets.py
+++ syng/gui/row_widgets.py
@@ -547,6 +547,7 @@ class RowWidget[T](QWidget):
         self.help = help
         self.description = description
         self._label = QLabel(description, self)
+        self._label.setToolTip(help)
         self.valueChanged.connect(self._set_internal_value)
         self.valueChanged.connect(self.set_value)
         self._input_widget = input_widget
